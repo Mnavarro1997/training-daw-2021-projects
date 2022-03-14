dc54598b190b4496412aa49f1e3d3c403d1d723f
a24956 <a24956@svalero.com>
2022-03-14 08:06:00 +0100

    project vue

--- ecommerce/frontend/src/views/About.vue
+++ ecommerce/frontend/src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div v-for="product, index in products" v-bind:key="product.id" style="display:flex; margin-top: 50px;">
+        <div class="imagen">
+          <img :src="product.mainImage" alt="img" width="250" height="250">
+        </div>
+        <div class="datos">
+          <div class="idname" style="text-align: left; margin-left: 20px; margin-top: 15px;">
+            ID: {{index}} - {{product.name}}
+          </div>
+          <div class="desc" style="text-align: left; margin-left: 20px; margin-top: 15px;">
+            {{product.description}} <br>
+          </div>
+          <div class="price" style="text-align: left; margin-left: 20px; margin-top: 15px;">
+            {{product.price}}€
+          </div>
+          <b-button variant="outline-primary">View</b-button>
+          <b-button variant="outline-primary" style="margin-left: 20px;">Add</b-button>
+        </div> 
+      </div>
   </div>
+  
 </template>
--- ecommerce/frontend/src/views/Cart.vue
+++ ecommerce/frontend/src/views/Cart.vue
@@ -5,10 +5,9 @@
       <router-link to="/cart">Cart</router-link>
     </div>
     <router-view/>
-    <div> 
-      
-    <h4>Productos</h4>
+    <h4>Carro de la compra</h4>
     <hr/>
+
       <div v-for="product, index in products" v-bind:key="product.id" style="display:flex; margin-top: 50px;">
         <div class="imagen">
           <img :src="product.mainImage" alt="img" width="250" height="250">
@@ -23,16 +22,12 @@
           <div class="price" style="text-align: left; margin-left: 20px; margin-top: 15px;">
             {{product.price}}€
           </div>
-          <b-button variant="outline-primary">View</b-button>
-          <b-button variant="outline-primary" style="margin-left: 20px;">Add</b-button>
-          <button v-on:onclick="añadir"> Añadir </button>
+          <button v-on:onclick="añadir" > Añadir </button>
           <button v-on:onclick="eliminar"> Eliminar </button>
         </div> 
       </div>
   </div>
-  </div>
 </template>
-
 <script>
 export default {
   components: {
@@ -47,15 +42,7 @@ export default {
   data() {
     return {
       products: [],
-      cart: []
-    }
-  },
-  methods : {
-    addCart(){
-      var products = []
-      products.push(new products())
-      alert('deseas añadir al carrito?')
-
+      cart: [],
     }
   },
   computed: {
@@ -65,26 +52,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
